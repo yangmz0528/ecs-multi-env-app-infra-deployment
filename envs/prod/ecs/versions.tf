acfa05a53ea52c7ca10f0024b8713850861de2a1
MINGZI\mingz <yang.mingzi0528@gmail.com>
2024-02-24 11:37:49 +0800

    update

--- envs/prod/ecs/versions.tf
+++ envs/prod/ecs/versions.tf
@@ -2,7 +2,7 @@ terraform {
     required_version = ">= 1.6"
     required_providers {
       aws = {
-        source = "hashicop/aws"
+        source = "hashicorp/aws"
         version = "~> 5.31.0"
       }
     }
